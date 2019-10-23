30b97489b9afa022b653a8b61f2830668c9f2f91
deshilearning <deshilearning@gmail.com>
2019-10-24 03:19:07 +0530

    Adding rest resorce

--- gitbashArt/src/main/java/com/gitbash/gitbashArt/GitbashArtApplication.java
+++ gitbashArt/src/main/java/com/gitbash/gitbashArt/GitbashArtApplication.java
@@ -2,7 +2,9 @@ package com.gitbash.gitbashArt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class GitbashArtApplication {
 
@@ -10,4 +12,8 @@ public class GitbashArtApplication {
 		SpringApplication.run(GitbashArtApplication.class, args);
 	}
 
+	@GetMapping("/name")
+	public String getName() {
+		return "Desh Deepak";
+	}
 }
