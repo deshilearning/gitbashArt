94898b39275e3f6949d82a893618aa84b1912d7c
deshilearning <deshilearning@gmail.com>
2019-10-24 03:05:03 +0530

    First commit

---
+++ gitbashArt/src/main/java/com/gitbash/gitbashArt/GitbashArtApplication.java
@@ -0,0 +1,13 @@
+package com.gitbash.gitbashArt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbashArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitbashArtApplication.class, args);
+	}
+
+}
