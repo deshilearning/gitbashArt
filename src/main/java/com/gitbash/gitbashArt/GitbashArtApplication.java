d0e4d9b95cdd6e5493e0106663397d25cdf37514
deshilearning <deshilearning@gmail.com>
2019-10-24 16:21:39 +0530

    calling git init from /c/learning/gitbashArtRepo/gitbashArt

--- src/main/java/com/gitbash/gitbashArt/GitbashArtApplication.java
+++ src/main/java/com/gitbash/gitbashArt/GitbashArtApplication.java
@@ -16,4 +16,48 @@ public class GitbashArtApplication {
 	public String getName() {
 		return "Desh Deepak";
 	}
+	
+	@GetMapping("/question")
+	public Question getQuestion() {
+		return new Question("LongAns", "1", "20", "What are the standard proc for Heroku?");
+	}
+	
+}
+
+class Question{
+	private String qType;
+	private String qNum;
+	private String qMarks;
+	private String qContent;
+	public Question(String qType, String qNum, String qMarks, String qContent) {
+		super();
+		this.qType = qType;
+		this.qNum = qNum;
+		this.qMarks = qMarks;
+		this.qContent = qContent;
+	}
+	public String getqType() {
+		return qType;
+	}
+	public void setqType(String qType) {
+		this.qType = qType;
+	}
+	public String getqNum() {
+		return qNum;
+	}
+	public void setqNum(String qNum) {
+		this.qNum = qNum;
+	}
+	public String getqMarks() {
+		return qMarks;
+	}
+	public void setqMarks(String qMarks) {
+		this.qMarks = qMarks;
+	}
+	public String getqContent() {
+		return qContent;
+	}
+	public void setqContent(String qContent) {
+		this.qContent = qContent;
+	}
 }
